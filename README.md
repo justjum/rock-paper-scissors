f712f6b129dc65d86579edde6b4ecb80d3fc339c
justjum <jumasaurusrex@gmail.com>
2023-07-12 18:54:42 +0800

    Write JS program for Rock Paper Scissors game *** Completely forgot to update on iterations ***
    Used "comments" process to write out what I'd want the code to do
    prior to starting to write - made it much easier.
    
    - included functions that called other functions
    - prompts can be used as function operators
    - tallying a score worked well
    - picking a random number Math.floor(Math.random()* max nubmer)
      so in this case max number was three. (result = 0,1,2 as Math.floor
      rounds down)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
 First Javascript Program
+
+This was fun!
+
+Got to play around with functions that called other functions, random number
+generation (Math.floor(Math.random()* max number)) and compiling of scores.
---
+++ script.js
@@ -0,0 +1,116 @@
+
+let result = '';
+let player = 0;
+let computer = 0;
+
+// Begin with function "getComputerChoice" - random return 'Rock','Paper' or 'Scissors'
+// Return of computerSelection
+
+function getComputerChoice() {
+
+    let computerNumber = Math.floor(Math.random()*3)
+
+    console.log(computerNumber)
+
+    switch(computerNumber) {
+        case 0: computerSelection = 'rock';
+        break;
+        case 1: computerSelection = 'paper';
+        break;
+        case 2: computerSelection = 'scissors';
+        break;
+    }
+
+    return(computerSelection)
+}
+
+
+
+
+// Write a function that plays a single round of rock, paper, scissors
+
+function playRound(playerSelection, computerSelection) {
+    //make the players selection case sensitive (doesn't yet check for correct wording)
+    playerSelection = playerSelection.toLowerCase();
+    
+    //nested if statements to check possible combinations.  A draw can be checked in one conditional so it's first. 
+    if (playerSelection === computerSelection) {
+        console.log("It's a draw!");
+        return (result = 'draw');
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            console.log("You lose!");
+            return(result = 'loss')
+        }
+        else {
+            console.log("Winner");
+            return(result = 'win')
+        }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            console.log("You lose!");
+            return(result = 'loss')
+        }
+        else {
+            console.log("Winner!");
+            return(result = 'win')
+        }
+    }
+    else {
+        if (computerSelection === 'rock') {
+            console.log("You lose!");
+            return(result = 'loss')
+        }
+        else {
+            console.log("Winner!");
+            return(result = 'win')
+        }
+    }
+
+}
+
+//playRound(prompt("Rock, Paper, or Scissors?"), computerSelection)
+
+
+//write a funcion game() which plays out three rounds, tabling the results to declare a winner
+
+
+function game() {
+    let computer = 0
+    let player = 0
+    let x = 1
+    // for statement to play out five rounds
+    for (x; x <= 5; x++) {
+        //playround function, prompting for user input, randomly generating new computer choice
+        playRound(prompt("Rock, Paper, or Scissors?"), getComputerChoice())
+        //feed in return "result" from playround, increment score.
+        if (result === 'win') {
+            player++;
+            console.log(`${player} - ${computer}`)
+        }
+        else if (result === 'loss') {
+            computer++;
+            console.log(`${player} - ${computer}`)
+        }
+    }
+    //alert final score after playing out five rounds
+    alert(`Player=${player}    Computer=${computer}`)
+}
+
+game();
+
+
+
+
+
+
+
+
+
+
+    // Prompt user for choice 'rock, paper, or scissors': return of playerSelection, case insensitive
+
+
+    // if/else loop for computerSelection nested in if/else condition for playerSelection
